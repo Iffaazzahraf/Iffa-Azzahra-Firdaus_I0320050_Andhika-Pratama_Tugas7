870739cfdea897c0e97d20c08867320bd450f99f
Iffaazzahraf <80144840+Iffaazzahraf@users.noreply.github.com>
2021-04-16 11:02:50 +0700

    Add files via upload

---
+++ I0320050_soal 1_tugas 7.py
@@ -0,0 +1,20 @@
+#program menggunakan fungsi string
+
+a = input('Masukkan kata atau kalimat\n>> ')
+
+#kapitalisasi huruf pertama kata pertama
+print('mengapitalisasikan huruf pertama')
+b = a.capitalize()
+print(b)
+
+#center
+print('membuat kalimat di tengah ')
+c=a.center(50,'*')
+print(c)
+
+#besar
+print('membuat semua menjadi huruf besar')
+d = a.upper()
+print(d)
+
+
